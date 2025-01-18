8340728f7ccce44628d0bd22d9b3e34ea312f534
ABHI backend <abhi708278@gmail.com>
2025-01-18 13:59:46 +0530

    port change

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-require('dotenv').config();
-
-// const app = require('./app');   
+require('dotenv').config(); 
 const app = require('./index');
-
 const connection = require('./DatabaseConnection/connection');
-const PORT = process.env.PORT;
+// const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT : ${PORT}`);
 });
+
+
 
 
 
